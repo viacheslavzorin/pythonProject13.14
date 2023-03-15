c3e3d3f0191d795a8283085a6e11bbc7ba8ced1b
viacheslavzorin <zorin-viacheslav@rambler.ru>
2023-03-15 23:55:12 +0300

    домашка 15.3

---
+++ scr/linked_list.py
@@ -0,0 +1,39 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next_node = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_beginning(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next_node = self.head
+            self.head = new_node
+
+    def insert_at_and(self, data):
+        new_node = Node(data)
+        if self.tail is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next_node = new_node
+            self.tail = new_node
+
+    def print_ll(self):
+        ll_string = ''
+        node = self.head
+        if node is None:
+            print(None)
+        while node:
+            ll_string += f' {str(node.data)} ->'
+            node = node.next_node
+
+        ll_string += ' None'
+        print(ll_string)
